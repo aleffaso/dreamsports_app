b4ab712ef9c0120ddf1303365f66edb07e24984b
Aleff <aleff.a.s.o@gmail.com>
2023-04-19 19:41:48 -0300

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
